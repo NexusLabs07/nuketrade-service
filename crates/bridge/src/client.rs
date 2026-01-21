38f1a3d45aa226cff7911e03d9d76da21a4e6014
Vaibhav Arora <aroravaibhav817@gmail.com>
2026-01-21 16:23:43 +0530

    feat: add relay api calls

---
+++ crates/bridge/src/client.rs
@@ -0,0 +1,92 @@
+use anyhow::Result;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::RELAY_API_URL;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QuoteRequest {
+    user: String,
+    #[serde(rename = "originChainId")]
+    origin_chain_id: u64,
+    #[serde(rename = "destinationChainId")]
+    destination_chain_id: u64,
+    #[serde(rename = "originCurrency")]
+    origin_currency: String,
+    #[serde(rename = "destinationCurrency")]
+    destination_currency: String,
+    amount: String,
+    #[serde(rename = "tradeType")]
+    trade_type: String,
+    #[serde(rename = "usePermit")]
+    use_permit: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PermitRequest {
+    signature: String,
+    kind: String,
+    #[serde(rename = "requestId")]
+    request_id: String,
+    api: String,
+}
+
+pub type RelayResponse = Value;
+
+pub struct BridgeClient {
+    pub client: Client,
+    pub base_url: String,
+}
+
+impl BridgeClient {
+    pub fn new() -> Self {
+        Self {
+            client: Client::new(),
+            base_url: RELAY_API_URL.to_string(),
+        }
+    }
+
+    pub async fn quote(&self, quote_request: QuoteRequest) -> Result<RelayResponse> {
+        let response = self
+            .client
+            .post(format!("{}{}", self.base_url, "/quote/v2"))
+            .json(&quote_request)
+            .send()
+            .await?;
+
+        let data: RelayResponse = match response.json().await {
+            Ok(d) => d,
+            Err(err) => {
+                log::error!(
+                    "Failed to fetch quote using relay. Failed with error: {:?}",
+                    err
+                );
+
+                return Err(anyhow::Error::msg("Failed to submit permit using relay"));
+            }
+        };
+
+        Ok(data)
+    }
+
+    pub async fn execute_permit(&self, permit_request: PermitRequest) -> Result<RelayResponse> {
+        let response = self
+            .client
+            .post(format!("{}{}", self.base_url, "/execute/permits"))
+            .json(&permit_request)
+            .send()
+            .await?;
+
+        let data: RelayResponse = match response.json().await {
+            Ok(d) => d,
+            Err(err) => {
+                log::error!("Failed to submit permit. Failed with error: {:?}", err);
+
+                return Err(anyhow::Error::msg("Failed to submit permit using relay"));
+            }
+        };
+
+        Ok(data)
+    }
+}
---
+++ crates/bridge/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod client;
+
+pub const RELAY_API_URL: &'static str = "https://api.relay.link";
+
+pub const SUPPORTED_CHAINS: [u64; 3] = [8453, 1337, 792703809];
