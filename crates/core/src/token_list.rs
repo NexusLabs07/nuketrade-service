467b86a1519c4af577b9a5fec00243c9c397bd9a
Aman-Mandal <002amankumar@gmail.com>
2026-01-26 21:19:59 +0530

    fix: removed bonk and pepe

--- crates/core/src/token_list.rs
+++ crates/core/src/token_list.rs
@@ -1,6 +1,6 @@
-pub const TOKEN_LIST: [&str; 44] = [
+pub const TOKEN_LIST: [&str; 42] = [
     "2Z", "AAVE", "ADA", "ARB", "ASTER", "AVAX", "BCH", "BNB", "BTC", "CRV", "DOGE", "ENA", "ETH",
-    "FARTCOIN", "HYPE", "ICP", "JUP", "kBONK", "kPEPE", "LDO", "LINK", "LIT", "LTC", "MEGA", "MON",
-    "NEAR", "PAXG", "PENGU", "PUMP", "SOL", "STRK", "SUI", "TAO", "TRUMP", "UNI", "VIRTUAL", "WIF",
-    "WLD", "WLFI", "XMR", "XPL", "XRP", "ZEC", "ZK",
+    "FARTCOIN", "HYPE", "ICP", "JUP", "LDO", "LINK", "LIT", "LTC", "MEGA", "MON", "NEAR", "PAXG",
+    "PENGU", "PUMP", "SOL", "STRK", "SUI", "TAO", "TRUMP", "UNI", "VIRTUAL", "WIF", "WLD", "WLFI",
+    "XMR", "XPL", "XRP", "ZEC", "ZK",
 ];
