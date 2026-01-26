1c4baa421c05de4aaf7ca64ffcc79486de19d823
Vaibhav Arora <aroravaibhav817@gmail.com>
2026-01-26 13:50:05 +0530

    fix: pacifica feed

--- crates/pacifica/src/apis/mod.rs
+++ crates/pacifica/src/apis/mod.rs
@@ -1,1 +1,3 @@
 pub mod user;
+
+//TODO: Create metadata for pacifica and use default leverage if not available in the settings
--- crates/pacifica/src/ws.rs
+++ crates/pacifica/src/ws.rs
@@ -82,7 +82,7 @@ pub async fn start_pacifica_funding_feed(
     let sub = json!({
         "method": "subscribe",
         "params": {
-            "source": "price"
+            "source": "prices"
         }
     });
 
@@ -98,7 +98,7 @@ pub async fn start_pacifica_funding_feed(
                         Ok(msg) => {
                             let (keep_alive, new_snapshot) = handle_ws_message(msg, &mut write).await;
                             if let Some(snapshot) = new_snapshot {
-                                log::info!("Received new snapshot from Pacifica");
+                                log::info!("Received new snapshot from Pacifica: {:?}", snapshot);
                                 last_update = Instant::now();
                                 for item in snapshot.into_iter() {
                                     last_snapshot.insert(item.0, (item.1, item.2));
