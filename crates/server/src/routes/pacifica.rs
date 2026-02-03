553d4a48f24a3067afa72567fc8faf27069da31f
Vaibhav Arora <aroravaibhav817@gmail.com>
2026-02-03 12:37:50 +0530

    fix: add depost to pacifica

--- crates/server/src/routes/pacifica.rs
+++ crates/server/src/routes/pacifica.rs
@@ -1,10 +1,15 @@
-use axum::routing::get;
+use axum::routing::{get, post};
 
-use crate::{AppState, controller::pacifica::get_user_open_positions};
+use crate::{
+    AppState,
+    controller::pacifica::{bridge_to_pacifica, get_user_open_positions},
+};
 
 pub fn routes() -> axum::Router<AppState> {
-    axum::Router::new().route(
-        "/open-positions/{user_solana_address}",
-        get(get_user_open_positions),
-    )
+    axum::Router::new()
+        .route(
+            "/open-positions/{user_solana_address}",
+            get(get_user_open_positions),
+        )
+        .route("/deposit", post(bridge_to_pacifica))
 }
