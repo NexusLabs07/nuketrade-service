7730ab0cef7c0a8ec5693ffca048dc96adbe0bdd
Vaibhav Arora <aroravaibhav817@gmail.com>
2026-02-20 15:55:17 +0530

    feat: add method for bridge

--- crates/server/src/features/withdraw/routes.rs
+++ crates/server/src/features/withdraw/routes.rs
@@ -6,7 +6,7 @@ use axum::{
 use crate::{
     AppState,
     features::withdraw::controller::{
-        create_withdraw_transaction, create_withdrawal_intent, get_next_action,
+        bridge, create_withdraw_transaction, create_withdrawal_intent, get_next_action,
         get_withdrawal_intent_detail, list_user_withdrawal_intents, report_action_result,
     },
 };
@@ -15,6 +15,7 @@ pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/create-intent", post(create_withdrawal_intent))
         .route("/transaction", post(create_withdraw_transaction))
+        .route("/bridge", post(bridge))
         .route("/{id}", get(get_withdrawal_intent_detail))
         .route("/{id}/next-action", get(get_next_action))
         .route("/{id}/action-result", post(report_action_result))
