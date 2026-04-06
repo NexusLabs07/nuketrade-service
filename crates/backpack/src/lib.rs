7a63878ae79d9a6de7fb41ca2004349e2c9d893b
Aditya Mandal <mandaladitya1614@gmail.com>
2026-04-06 20:49:29 +0530

    init backpack

---
+++ crates/backpack/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod helpers;
+pub mod types;
+pub mod ws;
+
+pub const BACKPACK_WS_URL: &str = "wss://ws.backpack.exchange";
+pub const BACKPACK_HTTP_URL: &str = "https://api.backpack.exchange/api/v1";
