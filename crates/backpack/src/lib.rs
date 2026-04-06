40034414c3ec48ed0406ce03886dbbf304627b58
Aditya Mandal <mandaladitya1614@gmail.com>
2026-04-06 22:32:27 +0530

    added backpack exchange client

--- crates/backpack/src/lib.rs
+++ crates/backpack/src/lib.rs
@@ -1,6 +1,9 @@
+pub mod exchange_impl;
 pub mod helpers;
 pub mod types;
 pub mod ws;
+
+pub use exchange_impl::BackpackExchange;
 
 pub const BACKPACK_WS_URL: &str = "wss://ws.backpack.exchange";
 pub const BACKPACK_HTTP_URL: &str = "https://api.backpack.exchange/api/v1";
